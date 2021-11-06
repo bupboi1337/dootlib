3e2fedd9198d83252d912e6587e4053ca555cfee
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-11-05 22:10:32 -0700

    added comments and a load of functions for devs using this library

--- main.ts
+++ main.ts
@@ -1,45 +1,64 @@
+// resume the main part of the "doot" Function
+function doot_again () {
+    useless_variable = 0
+}
+// play doot
 function doot () {
     for (let index = 0; index < 99999; index++) {
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(330, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(294, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(233, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(233, music.beat(BeatFraction.Quarter))
-        music.playTone(247, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(330, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(294, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        basic.pause(30)
-        music.playTone(165, music.beat(BeatFraction.Quarter))
-        music.playTone(233, music.beat(BeatFraction.Whole))
-        basic.pause(100)
+        if (soup == 1) {
+            break;
+waitin = 1
+        } else {
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(233, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(233, music.beat(BeatFraction.Quarter))
+            music.playTone(247, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(262, music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            basic.pause(30)
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(233, music.beat(BeatFraction.Whole))
+            basic.pause(100)
+        }
     }
 }
+// stop the main functionality of the "doot" function
+function stahp () {
+    soup = 1
+    return 0
+}
+// boot-loader for the "doot" function
+let waitin = 0
+let soup = 0
+let useless_variable = 0
 basic.showLeds(`
     . # # # .
     . # . . .
@@ -48,3 +67,11 @@ basic.showLeds(`
     . # . . .
     `)
 doot()
+// forever script to make the "doot_again" function work
+basic.forever(function () {
+    while (waitin == 1 && useless_variable == 1) {
+        useless_variable = 0
+        waitin = 0
+        soup = 0
+    }
+})
